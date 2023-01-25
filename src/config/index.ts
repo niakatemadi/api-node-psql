aa33eb65b3a0d60bb2e1a4aa5a753625d16cb99b
Madi niakate <madi.niakate@accenture.com>
2023-01-25 10:41:54 +0100

    azaz

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 import merge from "lodash.merge";
  
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
  
 const stage = process.env.STAGE || "local";
 let envConfig;
